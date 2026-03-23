6648ea2776a8dcedd21c0dac55ac40d1301f075e
Tamir Bahar <tamir.bahar@gmail.com>
2026-03-23 22:19:22 +0200

    Add system tray icon with Exit menu

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace EasyEyes;
+
+public partial class App : Application
+{
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
+using Forms = System.Windows.Forms;
 
 namespace EasyEyes;
 
@@ -18,18 +20,46 @@ public partial class MainWindow : Window
     [DllImport("user32.dll")]
     private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 
+    private Forms.NotifyIcon _trayIcon = null!;
+
     public MainWindow()
     {
         InitializeComponent();
         SourceInitialized += OnSourceInitialized;
+        InitializeTrayIcon();
+    }
+
+    private void InitializeTrayIcon()
+    {
+        _trayIcon = new Forms.NotifyIcon
+        {
+            Icon = SystemIcons.Application,
+            Text = "Easy Eyes",
+            Visible = true
+        };
+
+        var menu = new Forms.ContextMenuStrip();
+        menu.Items.Add("Exit", null, (_, _) =>
+        {
+            _trayIcon.Visible = false;
+            _trayIcon.Dispose();
+            Application.Current.Shutdown();
+        });
+
+        _trayIcon.ContextMenuStrip = menu;
     }
 
     private void OnSourceInitialized(object? sender, EventArgs e)
     {
         var hwnd = new WindowInteropHelper(this).Handle;
         int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-        // WS_EX_TRANSPARENT makes the window click-through
-        // WS_EX_TOOLWINDOW hides it from Alt+Tab
         SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW);
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _trayIcon.Visible = false;
+        _trayIcon.Dispose();
+        base.OnClosed(e);
     }
 }
