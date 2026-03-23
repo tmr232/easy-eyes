0d48fa6ea68c507d3ea516665b00a309729705a0
Tamir Bahar <tamir.bahar@gmail.com>
2026-03-23 22:11:33 +0200

    Add fullscreen transparent click-through overlay with radial gradient

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace EasyEyes;
+
+public partial class MainWindow : Window
+{
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_TRANSPARENT = 0x00000020;
+    private const int WS_EX_LAYERED = 0x00080000;
+    private const int WS_EX_TOOLWINDOW = 0x00000080;
+
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        SourceInitialized += OnSourceInitialized;
+    }
+
+    private void OnSourceInitialized(object? sender, EventArgs e)
+    {
+        var hwnd = new WindowInteropHelper(this).Handle;
+        int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+        // WS_EX_TRANSPARENT makes the window click-through
+        // WS_EX_TOOLWINDOW hides it from Alt+Tab
+        SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW);
+    }
+}
